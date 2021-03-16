1aec74ddfe9e6e5865425545d7f5c5545b0bcf17
Behnoosh <behnoosh.beheshti@gmail.com>
2021-03-16 18:58:44 +0100

    Add hourly forecast to current button

--- src/app.js
+++ src/app.js
@@ -128,6 +128,9 @@ function showcurrentLocation(position) {
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+   axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentPosition(event) {
